96fcdf15154e2394c7391d4543517ef91d95e5cc
Rus <punyarit07@gmail.com>
2022-05-15 10:48:59 +0700

    update

---
+++ paint2dGraphic/Main.java
@@ -0,0 +1,11 @@
+package paint2dGraphic;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new MyFrame();
+
+	}
+
+}
---
+++ simple2DAnimation/Main.java
@@ -0,0 +1,10 @@
+package simple2DAnimation;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		new MyFrame();
+	}
+
+}
